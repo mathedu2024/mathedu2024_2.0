000ae9b545d3bcb3d6cb271e3af0dc9f52362942
mathedu2024 <mathedu2024.class@gmail.com>
2026-04-06 12:02:39 +0800

    ver2.1.3

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // 引入 getFirestore
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); // 初始化 Firestore 實例
 
-export { app, auth };
+export { app, auth, db }; // 將 db 加入匯出列表
